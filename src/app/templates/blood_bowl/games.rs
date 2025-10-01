1f2d949efb1f3680fa0d58d6b1459f9275aa6067
Eric Vogt <eric.vogt.pro@gmail.com>
2025-10-01 11:40:22 +0200

    events reorganized

--- src/app/templates/blood_bowl/games.rs
+++ src/app/templates/blood_bowl/games.rs
@@ -1,3 +1,6 @@
+use crate::app::templates::blood_bowl::games::events::{
+    GameSequence, PostGameSequence, PreGameSequence,
+};
 use crate::app::templates::blood_bowl::teams::{TeamCard, TeamSelector};
 use crate::app::templates::{AlertMessage, NavigationBar};
 use crate::data::blood_bowl::games::GameSummary;
@@ -7,18 +10,14 @@ use crate::errors::AppError;
 use crate::AppState;
 use askama::Template;
 use askama_web::WebTemplate;
-use blood_bowl_rs::actions::Success;
-use blood_bowl_rs::events::GameEvent;
 use blood_bowl_rs::games::Game;
 use blood_bowl_rs::games::GameStatus;
-use blood_bowl_rs::inducements::{Inducement, TreasuryAndPettyCash};
-use blood_bowl_rs::injuries::Injury;
 use blood_bowl_rs::players::{Player, PlayerStatistics};
-use blood_bowl_rs::prayers::PrayerToNuffle;
 use blood_bowl_rs::teams::Team;
 use blood_bowl_rs::translation::TranslatedName;
-use blood_bowl_rs::translation::TypeName;
 use blood_bowl_rs::weather::Weather;
+
+pub mod events;
 
 #[derive(Template, WebTemplate)]
 #[template(path = "blood_bowl/games/games_page.html")]
@@ -107,50 +106,11 @@ impl GamePage {
 
         let game_date = game.game_at.format("%d/%m/%Y à %H:%M").to_string();
 
-        let mut pre_game_sequence: Option<PreGameSequence> = None;
-        let mut post_game_sequence: Option<PostGameSequence> = None;
-
-        if editable {
-            let (first_team_money, second_team_money) = game.teams_money_left()?;
-
-            let (first_team_buyable_inducements, second_team_buyable_inducements) =
-                game.inducements_buyable_by_teams()?;
-
-            let (first_team_inducements, second_team_inducements) = game.teams_inducements();
-
-            let (first_team_prayers, second_team_prayers) = game.teams_prayers();
-
-            pre_game_sequence = Some(PreGameSequence {
-                game: game.clone(),
-                editable,
-                first_team_money,
-                second_team_money,
-                first_team_buyable_inducements,
-                second_team_buyable_inducements,
-                first_team_inducements,
-                second_team_inducements,
-                first_team_prayers,
-                second_team_prayers,
-            });
-
-            post_game_sequence = Some(PostGameSequence {});
-        }
-
-        let game_sequence = GameSequence {
-            game: game.clone(),
-            editable,
-            game_controller: GameEventController {
-                game: game.clone(),
-                first_team_event_controller: TeamEventController {
-                    game: game.clone(),
-                    team: game.first_team.clone(),
-                },
-                second_team_event_controller: TeamEventController {
-                    game: game.clone(),
-                    team: game.second_team.clone(),
-                },
-            },
-        };
+        let pre_game_sequence: Option<PreGameSequence> =
+            PreGameSequence::try_from_game(game.clone(), editable)?;
+        let post_game_sequence: Option<PostGameSequence> =
+            PostGameSequence::try_from_game(game.clone(), editable)?;
+        let game_sequence = GameSequence::from_game(game.clone(), editable);
 
         let game_statistics = GameStatistics {
             game: game.clone(),
@@ -179,54 +139,12 @@ impl GamePage {
 }
 
 #[derive(Template, WebTemplate)]
-#[template(path = "blood_bowl/games/pre_game_sequence.html")]
-struct PreGameSequence {
-    game: Game,
-    editable: bool,
-    first_team_money: TreasuryAndPettyCash,
-    second_team_money: TreasuryAndPettyCash,
-    first_team_buyable_inducements: Vec<Inducement>,
-    second_team_buyable_inducements: Vec<Inducement>,
-    first_team_inducements: Vec<Inducement>,
-    second_team_inducements: Vec<Inducement>,
-    first_team_prayers: Vec<PrayerToNuffle>,
-    second_team_prayers: Vec<PrayerToNuffle>,
-}
-
-#[derive(Template, WebTemplate)]
-#[template(path = "blood_bowl/games/game_sequence.html")]
-struct GameSequence {
-    game: Game,
-    editable: bool,
-    game_controller: GameEventController,
-}
-
-#[derive(Template, WebTemplate)]
-#[template(path = "blood_bowl/games/game_event_controller.html")]
-struct GameEventController {
-    game: Game,
-    first_team_event_controller: TeamEventController,
-    second_team_event_controller: TeamEventController,
-}
-
-#[derive(Template, WebTemplate)]
-#[template(path = "blood_bowl/games/team_event_controller.html")]
-struct TeamEventController {
-    game: Game,
-    team: Team,
-}
-
-#[derive(Template, WebTemplate)]
 #[template(path = "blood_bowl/games/game_statistics.html")]
 struct GameStatistics {
     game: Game,
     first_team_players_statistics: Vec<(i32, Player, PlayerStatistics)>,
     second_team_players_statistics: Vec<(i32, Player, PlayerStatistics)>,
 }
-
-#[derive(Template, WebTemplate)]
-#[template(path = "blood_bowl/games/post_game_sequence.html")]
-struct PostGameSequence {}
 
 #[derive(Template, WebTemplate)]
 #[template(path = "blood_bowl/games/new_game_page.html")]
