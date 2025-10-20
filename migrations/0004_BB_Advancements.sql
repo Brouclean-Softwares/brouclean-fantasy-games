a9da6e67ba70a4ddbff0fc56d5ab0c7816561a91
Eric Vogt <eric.vogt.pro@gmail.com>
2025-10-20 14:36:38 +0200

    advancement choice

--- migrations/0004_BB_Advancements.sql
+++ migrations/0004_BB_Advancements.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS bb_players_advancements (
     player_id INTEGER NOT NULL REFERENCES bb_players ON DELETE CASCADE,
-    advancement VARCHAR NOT NULL,
+    advancement VARCHAR,
+    options_to_choose VARCHAR,
     star_player_points INTEGER NOT NULL,
     added_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
